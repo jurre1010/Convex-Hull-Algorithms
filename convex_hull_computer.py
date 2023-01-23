b25de22249d8277d8a26be7a01b2c9804c32e830
Sander <s.a.t.hageman@students.uu.nl>
2023-01-23 15:11:06 +0100

    update visuals and check_convex_hull

--- convex_hull_computer.py
+++ convex_hull_computer.py
@@ -1,3 +1,4 @@
+import json
 import matplotlib.pyplot as plt
 from copy import copy
 from random import random
@@ -17,6 +18,11 @@ class Point:
     def __sub__(self, other):
         return Point(self.x - other.x, self.y-other.y)
 
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __hash__(self):
+        return hash((self.x, self.y))
 
 def generate_points(n, a, d):
     points = []
@@ -31,8 +37,8 @@ def show_points(points, a, hull_points):
         x.append(points[i].x)
         y.append(points[i].y)
     plt.scatter(x, y)
-    plt.xlim(0, a)
-    plt.ylim(0, a)
+    plt.xlim(-1, a + 1)
+    plt.ylim(-1, a + 1)
 
     hx = []
     hy = []
@@ -41,6 +47,9 @@ def show_points(points, a, hull_points):
         hy.append(i.y)
     plt.scatter(hx, hy, color="orange")
     plt.plot(hx, hy, color="orange")
+
+    is_convex_hull = check_convex_hull(points, hull_points)
+    plt.title("Is convex hull: {}".format(is_convex_hull))
 
     plt.show()
 
@@ -151,15 +160,30 @@ def check_convex_hull(points, points_on_convex_hull):
     check_points_from_set = len(points_on_convex_hull) == (len([p for p in points_on_convex_hull if p in points]))
 
     # check if all points from the original set are to the right
+    not_ch_points = [p for p in points if p not in points_on_convex_hull]
     check = True
+
     for i in range(len(points_on_convex_hull) - 1):
-        for p in [p for p in points if p not in points_on_convex_hull]:
-            check = check and orientation(points_on_convex_hull[i], points_on_convex_hull[i+1], p) == 1
+        for p in not_ch_points:
+            if orientation(points_on_convex_hull[i], points_on_convex_hull[i+1], p) != 2:
+                continue # if it's colinear or to the right; accept
+            print("Error: orientation of point ({}) incorrect! Orientation: {} with {} and {}".format(p, orientation(points_on_convex_hull[i], points_on_convex_hull[i+1], p), points_on_convex_hull[i], points_on_convex_hull[i+1]))
+            check = False
+
+    if not check:
+        dmp = list(map(vars, points))
+        print(json.dumps(dmp))
 
     return check_points_from_set and check
 
 def main():
     points = generate_points(10, 10, 1)
+
+    # Error: orientation of point ((9.9, 9.3)) incorrect! Orientation: 2 with (9.8, 9.9) and (10.0, 5.2)
+    # jss = r'[{"x": 0.7, "y": 0.1}, {"x": 8.4, "y": 7.7}, {"x": 7.7, "y": 9.3}, {"x": 3.4, "y": 6.1}, {"x": 6.6, "y": 6.5}, {"x": 5.6, "y": 0.8}, {"x": 7.2, "y": 5.8}, {"x": 0.5, "y": 9.7}, {"x": 7.2, "y": 0.8}, {"x": 1.9, "y": 1.7}, {"x": 6.4, "y": 7.8}, {"x": 6.5, "y": 6.9}, {"x": 4.1, "y": 1.6}, {"x": 9.2, "y": 0.6}, {"x": 5.5, "y": 3.3}, {"x": 9.9, "y": 9.3}, {"x": 2.6, "y": 7.1}, {"x": 6.5, "y": 5.2}, {"x": 9.5, "y": 3.3}, {"x": 2.1, "y": 3.0}, {"x": 2.3, "y": 8.9}, {"x": 1.0, "y": 7.0}, {"x": 6.2, "y": 9.0}, {"x": 10.0, "y": 5.2}, {"x": 3.5, "y": 7.7}, {"x": 9.0, "y": 1.4}, {"x": 9.8, "y": 7.4}, {"x": 8.6, "y": 7.9}, {"x": 1.1, "y": 1.5}, {"x": 0.7, "y": 4.2}, {"x": 8.0, "y": 4.3}, {"x": 2.4, "y": 1.1}, {"x": 3.7, "y": 10.0}, {"x": 4.4, "y": 9.6}, {"x": 8.3, "y": 6.5}, {"x": 3.0, "y": 8.8}, {"x": 5.2, "y": 6.6}, {"x": 9.3, "y": 4.6}, {"x": 3.5, "y": 0.2}, {"x": 2.6, "y": 4.0}, {"x": 0.1, "y": 1.1}, {"x": 3.0, "y": 1.3}, {"x": 4.0, "y": 5.5}, {"x": 2.9, "y": 0.8}, {"x": 1.4, "y": 2.5}, {"x": 1.5, "y": 1.1}, {"x": 4.4, "y": 1.8}, {"x": 9.8, "y": 9.9}, {"x": 9.1, "y": 6.6}, {"x": 5.5, "y": 4.9}, {"x": 4.4, "y": 8.3}, {"x": 4.9, "y": 7.3}, {"x": 6.2, "y": 3.3}, {"x": 6.8, "y": 4.1}, {"x": 5.4, "y": 2.4}, {"x": 9.2, "y": 4.7}, {"x": 1.8, "y": 0.1}, {"x": 5.2, "y": 3.9}, {"x": 4.8, "y": 2.6}, {"x": 9.5, "y": 5.3}, {"x": 6.5, "y": 8.0}, {"x": 9.5, "y": 8.3}, {"x": 8.4, "y": 9.1}, {"x": 9.6, "y": 6.3}, {"x": 6.4, "y": 6.6}, {"x": 1.1, "y": 4.2}, {"x": 1.6, "y": 6.6}, {"x": 5.8, "y": 7.2}, {"x": 1.2, "y": 9.5}, {"x": 8.8, "y": 6.4}, {"x": 6.0, "y": 0.6}, {"x": 7.6, "y": 2.2}, {"x": 2.0, "y": 7.3}, {"x": 4.5, "y": 3.2}, {"x": 3.0, "y": 8.4}, {"x": 2.0, "y": 8.4}, {"x": 8.4, "y": 1.8}, {"x": 8.8, "y": 5.3}, {"x": 2.1, "y": 3.9}, {"x": 8.4, "y": 4.3}, {"x": 9.6, "y": 1.2}, {"x": 5.8, "y": 3.4}, {"x": 0.0, "y": 6.6}, {"x": 4.3, "y": 9.0}, {"x": 8.0, "y": 2.5}, {"x": 2.0, "y": 0.7}, {"x": 6.0, "y": 8.9}, {"x": 1.5, "y": 5.2}, {"x": 1.6, "y": 6.6}, {"x": 9.5, "y": 1.4}, {"x": 1.0, "y": 1.2}, {"x": 4.1, "y": 4.1}, {"x": 0.0, "y": 4.8}, {"x": 4.2, "y": 2.8}, {"x": 7.9, "y": 0.5}, {"x": 6.4, "y": 1.4}, {"x": 2.0, "y": 2.3}, {"x": 7.5, "y": 1.4}, {"x": 7.2, "y": 1.4}, {"x": 0.5, "y": 4.2}]'
+
+    # points = json.loads(jss, object_hook=lambda d:Point(d['x'], d['y']))
+
     sorted_points, points_on_convex_hull = convex_hull(points)
     print(check_convex_hull(list(reversed(sorted_points)), points_on_convex_hull))
     show_points(points, 10, points_on_convex_hull)
